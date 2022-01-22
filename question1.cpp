1ed9f8f5c36b5734ddc6b8c1a0193124bfa3658a
uzaircs <nikuzairsc@gmail.com>
2022-01-22 14:49:22 +0800

    question submission  done

--- question1.cpp
+++ question1.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int i,n;
+
+    float sum=0, avg;
+    float *p;
+
+
+    cout << "How many marks would you like to enter: ";
+    cin >> i;
+
+    p = new float [i];
+
+    for (n=0; n<i; n++)
+    {
+        cout << "Enter marks " << n+1 << ": ";
+        cin >> p[n];
+        sum += p[n];
+    }
+
+    cout << endl << "You have entered : ";
+
+    for (n=0; n<i; n++)
+    {
+        cout << p[n] << ", ";
+    }
+    cout << "\b\b";
+
+    avg = sum / i;
+
+
+    cout << endl << endl << "Sum of mark is : " << sum << endl;
+    cout << "Average marks is : " << (int)avg;
+
+
+
     return 0;
 }
